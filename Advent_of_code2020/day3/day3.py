b907a9f0c5aa6eaa69292d7c65118f19e9ff073c
kieran-raj <kieran@rajasansir.co.uk>
2020-12-10 21:26:00 +0000

    update

--- Advent_of_code2020/day3/day3.py
+++ Advent_of_code2020/day3/day3.py
@@ -1,14 +1,9 @@
 import os
 
-with open('input2.txt' , 'r') as f:
+with open('input.txt' , 'r') as f:
     lines = f.readlines()
 
-
 len_slope = len(lines)
-#min_row_length = len_slope * 3
-#for i in lines:
-#    with open('input3.txt', 'a') as f2:
-#       f2.write(i.strip() + (min_row_length * i.strip()) + i)
 
 
 def input3(move):
@@ -24,20 +19,19 @@ def read_input():
         return f3.readlines()
 
 
-right = [1, 3, 5, 7, 1]
-down = [1, 1, 1, 1, 2]
-
-
-ans = []
-for r, d in zip(right, down):
+def get_ans(r, d):
+    ans = []
     input3(r)
     start_position = 0
     num_open = 0
     num_trees = 0
     lines2 = read_input()
+    if start_position == 0:
+        num = 0
+    else:
+        num = d
     for i in range(len(lines2)):
-        random = i + (d - 1)
-
+        random = i + num
         if random < len(lines2):
             if lines2[random][start_position] == '.':
                 num_open += 1
@@ -45,15 +39,11 @@ for r, d in zip(right, down):
             else:
                 num_trees += 1
                 start_position += r
-    ans.append(num_trees)
+    return num_trees
+
+print(get_ans(1, 1) * get_ans(3, 1) * get_ans(5, 1) * get_ans(7, 1) * get_ans(1, 2))
 
 
 
-final_ans = 1
-for i in ans:
-    final_ans *= i
 
-
-print(ans)
-print(final_ans)        
 
