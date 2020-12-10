3759656052b7a659d59b2fcb61837ccceaf80283
kieran-raj <kieran@rajasansir.co.uk>
2020-12-10 21:00:10 +0000

    Part 2 attempt

--- Advent_of_code2020/day3/day3.py
+++ Advent_of_code2020/day3/day3.py
@@ -1,50 +1,59 @@
 import os
-os.remove('input3.txt')
 
-right = [1, 3, 5, 7, 1]
-down = [1, 1, 1, 1, 2]
-
-with open('input.txt' , 'r') as f:
+with open('input2.txt' , 'r') as f:
     lines = f.readlines()
 
 
 len_slope = len(lines)
-min_row_length = len_slope * 3
-os.remove('input3.txt')
-for i in lines:
-    with open('input3.txt', 'a') as f2:
-        f2.write(i.strip() + (min_row_length * i.strip()) + i)
+#min_row_length = len_slope * 3
+#for i in lines:
+#    with open('input3.txt', 'a') as f2:
+#       f2.write(i.strip() + (min_row_length * i.strip()) + i)
 
 
-with open('input3.txt', 'r') as f3:
-    lines2 = f3.readlines()
-    ans = []
-    num_open = 0
-    num_trees = 0
-    right = [1, 3, 5, 7, 1]
-    down = [1, 1, 1, 1, 2]
-    # for r, d in zip(right, down):
+def input3(move):
+    os.remove('input3.txt')
+    min_row_length = len_slope * move
+    for i in lines:
+        with open('input3.txt', 'a') as f2:
+            f2.write(i.strip() + (min_row_length * i.strip()) + i)
+
+
+def read_input():
+    with open('input3.txt', 'r') as f3:
+        return f3.readlines()
+
+
+right = [1, 3, 5, 7, 1]
+down = [1, 1, 1, 1, 2]
+
+
+ans = []
+for r, d in zip(right, down):
+    input3(r)
     start_position = 0
     num_open = 0
     num_trees = 0
+    lines2 = read_input()
     for i in range(len(lines2)):
         random = i + (d - 1)
-        if random <= len(lines2):
+
+        if random < len(lines2):
             if lines2[random][start_position] == '.':
-                num_open += 0
+                num_open += 1
                 start_position += r
             else:
                 num_trees += 1
                 start_position += r
     ans.append(num_trees)
 
-count_trees = 1
+
+
+final_ans = 1
 for i in ans:
-    count_trees *= i
+    final_ans *= i
 
 
-print(count_trees)
+print(ans)
+print(final_ans)        
 
-
-
-        
